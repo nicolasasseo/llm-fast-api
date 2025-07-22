d2d435d50abb07abe07580c4d328dd4760b31552
nicolasasseo <nicolasasseogithub@gmail.com>
2025-07-22 09:49:36 +0200

    Added loop to log better

--- stop_app.sh
+++ stop_app.sh
@@ -8,4 +8,14 @@ sudo pkill -f "uvicorn"
 pkill -f "ollama"
 sudo pkill -f "ollama"
 
+for LOGFILE in fastapi.log ollama.log; do
+    if [ -f "$LOGFILE" ]; then
+        if [ -s "$LOGFILE" ]; then
+            cp "$LOGFILE" "old$LOGFILE"
+        fi
+        > "$LOGFILE"
+    fi
+done
+
+
 echo "All Uvicorn and Ollama processes have been stopped." 
